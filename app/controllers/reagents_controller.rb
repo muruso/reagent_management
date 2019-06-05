74943cbea30662962403b70159c2a036cd533122
muruso <t.hiatus-s.ryo@docomo.ne.jp>
2019-06-06 00:32:01 +0900

    add create method

--- app/controllers/reagents_controller.rb
+++ app/controllers/reagents_controller.rb
@@ -11,4 +11,21 @@ class ReagentsController < ApplicationController
   def new
     @reagent = Reagent.new
   end
+
+  def create
+    @reagent = Reagent.new(reagent_params)
+    if @reagent.save
+      redirect_to @reagents
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+    def reagent_params
+      params.require(:reagent).permit(:name, :maker, :number,
+                                   :amount, :price)
+    end
+
 end
